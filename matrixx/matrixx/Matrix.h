f990602df42bda2e7bc05bd10cfe85d5e0a05209
FajeGame <147686621+FajeGame@users.noreply.github.com>
2025-02-15 11:56:25 +0700

    Add files via upload

---
+++ matrixx/matrixx/Matrix.h
@@ -0,0 +1,59 @@
+﻿#ifndef MATRIXX_MATRIX_H
+#define MATRIXX_MATRIX_H
+
+#include <iostream>
+#include <stdexcept>
+
+namespace Matrixx {
+    class Matrix {
+    private:
+        int rows_, cols_;
+        double** matrix_;
+
+        void AllocateMemory();
+        void CopyMatrix(const Matrix& other);
+        void FreeMemory();
+
+    public:
+        Matrix();
+        Matrix(int rows, int cols);
+        Matrix(const Matrix& other);
+        Matrix(Matrix&& other) noexcept;
+        ~Matrix();
+
+        Matrix& operator=(const Matrix& other);
+        Matrix& operator=(Matrix&& other) noexcept;
+
+        Matrix operator+(const Matrix& other) const;
+        Matrix operator-(const Matrix& other) const;
+        Matrix operator*(const Matrix& other) const;
+        Matrix operator*(const double num) const;
+
+        bool operator==(const Matrix& other) const;
+
+        Matrix& operator+=(const Matrix& other);
+        Matrix& operator-=(const Matrix& other);
+        Matrix& operator*=(const Matrix& other);
+        Matrix& operator*=(const double num);
+
+        double& operator()(int i, int j);
+
+        double Determinant() const;
+        Matrix CalcComplements() const;
+        Matrix InverseMatrix() const;
+        Matrix Transpose() const;
+
+        bool EqMatrix(const Matrix& other) const;
+
+        void SumMatrix(const Matrix& other);
+        void SubMatrix(const Matrix& other);
+        void MulNumber(double num);
+        void MulMatrix(const Matrix& other);
+        Matrix GetMinor(int row, int col) const;
+
+        void print() const;
+        void fill();
+    };
+}
+
+#endif
